cdc04c4602d3195b58f9d27c4773fe52586918f0
Timo Leitritz <42964574+TimoIllusion@users.noreply.github.com>
2022-12-31 17:31:28 +0100

    Refactor MLPMixer

---
+++ benchmark.py
@@ -0,0 +1,39 @@
+import time
+import json
+
+from simple_ai_benchmarking.mlpmixer_benchmark import MLPMixer
+
+def main():
+    
+    
+    workload = MLPMixer()
+    
+    workload.setup()
+    
+    t0 = time.time()
+    workload.train()
+    t0_delta = time.time() - t0
+    
+    t1 = time.time()
+    workload.eval()
+    t1_delta = time.time() - t1
+    
+    log = workload.build_log_dict()
+    
+    print("Elapsed time for training:", t0_delta)
+    print("Elapsed time for inference:", t1_delta)
+    
+    log["train_duration_s"] = t0_delta
+    log["eval_duration_s"] = t1_delta
+    
+    log["samples_per_second_training"] = log["num_samples_training"] / log["train_duration_s"]
+    log["samples_per_second_inference"] = log["num_samples_eval"] / log["eval_duration_s"]
+    
+    
+    print("Log info:", log)
+    with open("log.json", "w") as f:
+        json.dump(log, f, indent=4)
+    
+
+if __name__ == "__main__":
+    main()
