e0ff6daa0d35f66c430b2fe6b94e3294a9dfc72a
Timo <timoleitri@aol.com>
2024-04-12 18:57:53 +0200

    Feature: add tests for default saib-pt saib-tf commands

---
+++ tests/test_default_execution.py
@@ -0,0 +1,28 @@
+# Project Name: simple-ai-benchmarking
+# File Name: test_default_execution.py
+# Author: Timo Leitritz
+# Copyright (C) 2024 Timo Leitritz
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+from simple_ai_benchmarking.entrypoints import run_pt_benchmarks, run_tf_benchmarks
+
+
+def test_pt_benchmark() -> None:
+
+    run_pt_benchmarks()
+
+def test_tf_benchmark() -> None:
+
+    run_tf_benchmarks()
